72dbb8ea020cbf34bbe236bbd331fd8a4b0c7ede
Elijah wines <elijahwines84@yahoo.com>
2020-02-25 21:09:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,12 +12,4 @@ class Shoe
   end
 end
 
-grape_fives = Shoe.new("Jordan")
-grape_fives.cobble
-puts grape_fives.condition
-grape_fives.color = "Purple and White"
-puts grape_fives.color
-grape_fives.size = "size 11"
-puts grape_fives.size
-grape_fives.material = "Leather"
-puts grape_fives.material
+
